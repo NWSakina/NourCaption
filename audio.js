67c541d323d98b833d133d311ff58efb0373091b
NWSakina <farissziyad37@gmail.com>
2026-07-21 17:51:34 +0100

    Create audio.js

---
+++ audio.js
@@ -0,0 +1,11 @@
+export async function extractAudio(videoFile) {
+
+    if (!videoFile) {
+        throw new Error("لم يتم اختيار فيديو");
+    }
+
+    console.log("بدء استخراج الصوت...");
+
+    return videoFile;
+
+}
