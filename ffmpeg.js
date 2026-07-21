19e2d481a12ce384594aa81443bd7a0c60623cd9
NWSakina <farissziyad37@gmail.com>
2026-07-21 22:03:49 +0100

    Update FFmpeg core and wasm URLs to local paths

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,28 +1,17 @@
 import { FFmpeg } from "https://unpkg.com/@ffmpeg/ffmpeg@0.12.10/dist/esm/index.js";
-import { toBlobURL } from "https://unpkg.com/@ffmpeg/util@0.12.1/dist/esm/index.js";
 
 const ffmpeg = new FFmpeg();
 
 let loaded = false;
 
 export async function loadFFmpeg() {
-
     if (loaded) {
         return ffmpeg;
     }
 
-    const baseURL =
-        "https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm";
-
     await ffmpeg.load({
-        coreURL: await toBlobURL(
-            `${baseURL}/ffmpeg-core.js`,
-            "text/javascript"
-        ),
-        wasmURL: await toBlobURL(
-            `${baseURL}/ffmpeg-core.wasm`,
-            "application/wasm"
-        )
+        coreURL: "./ffmpeg/ffmpeg-core.js",
+        wasmURL: "./ffmpeg/ffmpeg-core.wasm"
     });
 
     loaded = true;
