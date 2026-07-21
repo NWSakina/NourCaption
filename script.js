49d00a39ad7a8cca279b438783186220b9370e8c
NWSakina <farissziyad37@gmail.com>
2026-07-21 01:27:20 +0100

    Add video file selection functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function testApp() {
-  alert("NourCaption يعمل");
-}
+const videoInput = document.querySelector('input[type="file"]');
+
+videoInput.addEventListener("change", function () {
+  const file = this.files[0];
+
+  if (file) {
+    alert("تم اختيار الفيديو: " + file.name);
+  }
+});
