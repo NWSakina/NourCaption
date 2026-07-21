d3e8cc903047dfb2792f66ab09417688ec1da024
NWSakina <farissziyad37@gmail.com>
2026-07-21 13:34:25 +0100

    Update FFmpeg loading logic and worker URL

--- ffmpeg.js
+++ ffmpeg.js
@@ -4,6 +4,7 @@ import { toBlobURL } from "https://cdn.jsdelivr.net/npm/@ffmpeg/util@0.12.1/+esm
 const ffmpeg = new FFmpeg();
 
 export async function loadFFmpeg() {
+
   if (ffmpeg.loaded) {
     return ffmpeg;
   }
@@ -11,7 +12,13 @@ export async function loadFFmpeg() {
   const baseURL =
     "https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.6/dist/esm";
 
+  const ffmpegWorker = await toBlobURL(
+    "https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.12.10/dist/esm/worker.js",
+    "text/javascript"
+  );
+
   await ffmpeg.load({
+    classWorkerURL: ffmpegWorker,
     coreURL: await toBlobURL(
       `${baseURL}/ffmpeg-core.js`,
       "text/javascript"
