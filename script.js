ee5bc68699f0827885cd6dcb64d0d7a5fe229373
NWSakina <farissziyad37@gmail.com>
2026-07-21 01:58:38 +0100

    Add video preview functionality to video input

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const videoInput = document.getElementById("videoInput");
 const fileName = document.getElementById("fileName");
+const videoPreview = document.getElementById("videoPreview");
 
 videoInput.addEventListener("change", function () {
   const file = this.files[0];
 
   if (file) {
     fileName.textContent = "الفيديو المختار: " + file.name;
+
+    const videoURL = URL.createObjectURL(file);
+    videoPreview.src = videoURL;
   } else {
     fileName.textContent = "لم يتم اختيار فيديو";
+    videoPreview.src = "";
   }
 });
-
-function testApp() {
-  alert("NourCaption يعمل");
-}
