ef576cec53f0030c6210bc119698fa84a58928c9
NWSakina <farissziyad37@gmail.com>
2026-07-22 17:43:10 +0100

    Update ffmpeg.js

--- ffmpeg.js
+++ ffmpeg.js
@@ -10,11 +10,15 @@ export async function loadFFmpeg() {
 
     ffmpeg = new FFmpeg();
 
+    const baseURL = "./ffmpeg/";
+
     await ffmpeg.load({
-        coreURL: "./ffmpeg/ffmpeg-core.js",
-        wasmURL: "./ffmpeg/ffmpeg-core.wasm",
-        workerURL: "./ffmpeg/worker.js"
+        coreURL: baseURL + "ffmpeg-core.js",
+        wasmURL: baseURL + "ffmpeg-core.wasm",
+        workerURL: baseURL + "worker.js"
     });
 
     return ffmpeg;
 }
+
+export default ffmpeg;
