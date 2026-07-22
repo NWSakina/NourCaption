b2d1ded0653db5e78adc35330b6758ec243b4fe1
NWSakina <farissziyad37@gmail.com>
2026-07-22 03:43:03 +0000

    Fix FFmpeg local worker

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,22 +1,21 @@
 import { FFmpeg } from "https://unpkg.com/@ffmpeg/ffmpeg@0.12.10/dist/esm/index.js";
+import { toBlobURL } from "https://unpkg.com/@ffmpeg/util@0.12.1/dist/esm/index.js";
 
 const ffmpeg = new FFmpeg();
 
 let loaded = false;
 
 export async function loadFFmpeg() {
-    if (loaded) {
-        return ffmpeg;
-    }
+    if (loaded) return ffmpeg;
 
-    await ffmpeg.load({
-        coreURL: "./ffmpeg/ffmpeg-core.js",
-        wasmURL: "./ffmpeg/ffmpeg-core.wasm"
-    });
+        await ffmpeg.load({
+                coreURL: await toBlobURL("./ffmpeg/ffmpeg-core.js", "text/javascript"),
+                        wasmURL: await toBlobURL("./ffmpeg/ffmpeg-core.wasm", "application/wasm"),
+                                workerURL: await toBlobURL("./ffmpeg/worker.js", "text/javascript")
+                                    });
 
-    loaded = true;
+                                        loaded = true;
+                                            return ffmpeg;
+                                            }
 
-    return ffmpeg;
-}
-
-export default ffmpeg;
+                                            export default ffmpeg;
